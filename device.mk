5cf22096987e41044985324854535efa9734d2e9
lindwurm <admin@lindwurm.biz>
2016-11-24 22:55:27 +0900

    op3: Disable BLOCK_BASED_OTA

--- device.mk
+++ device.mk
@@ -131,6 +131,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
+# Not BLOCK BASED
+BLOCK_BASED_OTA := false
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
